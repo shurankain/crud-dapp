ccda753445935d37c45f2b2ac76e89df7d07d943
Oleksandr_Husiev <ohusiev@icloud.com>
2024-11-24 22:07:57 +0100

    Added update journal logic

--- anchor/programs/crudapp/src/lib.rs
+++ anchor/programs/crudapp/src/lib.rs
@@ -15,6 +15,12 @@ pub mod crudapp {
         journal_entry.message = message;
         Ok(())
     }
+
+    pub fn update_journal_entry(ctx: Context<UpdateEntry>, _title: String, message: String) -> Result<()> {
+        let journal_entry = &mut ctx.accounts.journal_entry;
+        journal_entry.message = message;
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
@@ -26,6 +32,23 @@ pub struct CreateEntry<'info> {
         bump,
         space = 8 + JournalEntryState::INIT_SPACE,
         payer = owner,)]
+    pub journal_entry: Account<'info, JournalEntryState>,
+    #[account(mut)]
+    pub owner: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(title: String)]
+pub struct UpdateEntry<'info> {
+    #[account(
+        mut,
+        seeds = [title.as_bytes(), owner.key().as_ref()],
+        bump,
+        realloc = 8 + JournalEntryState::INIT_SPACE,
+        realloc::payer = owner,
+        realloc::zero = true,
+    )]
     pub journal_entry: Account<'info, JournalEntryState>,
     #[account(mut)]
     pub owner: Signer<'info>,
