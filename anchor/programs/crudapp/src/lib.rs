776e21301a156209fe62fd3bc059f2fe3aaadc82
Oleksandr_Husiev <ohusiev@icloud.com>
2024-11-24 22:17:32 +0100

    Added delete journal logic

--- anchor/programs/crudapp/src/lib.rs
+++ anchor/programs/crudapp/src/lib.rs
@@ -21,6 +21,10 @@ pub mod crudapp {
         journal_entry.message = message;
         Ok(())
     }
+
+    pub fn delete_journal_entry(_ctx: Context<DeleteEntry>, _title: String) -> Result<()> {
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
@@ -48,6 +52,21 @@ pub struct UpdateEntry<'info> {
         realloc = 8 + JournalEntryState::INIT_SPACE,
         realloc::payer = owner,
         realloc::zero = true,
+    )]
+    pub journal_entry: Account<'info, JournalEntryState>,
+    #[account(mut)]
+    pub owner: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(title: String)]
+pub struct DeleteEntry<'info> {
+    #[account(
+        mut,
+        seeds = [title.as_bytes(), owner.key().as_ref()],
+        bump,
+        close = owner,
     )]
     pub journal_entry: Account<'info, JournalEntryState>,
     #[account(mut)]
